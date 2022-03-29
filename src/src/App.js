bd475c4b4fe8b8cb10dd77c1dc2f17b9e98464d8
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-29 13:10:38 +0200

    First Commit

---
+++ src/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  
+  
+  
+  
+  
+  
+  
+  ereturn (
+    <>
+    <h1>Texto:</h1>
+    <textarea onChange={changeTextHandler}></textarea>
+    </>
+  )
+}
+
+export default App;
